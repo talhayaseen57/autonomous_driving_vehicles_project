91cdb15f56bffa0ed1caf213ac73f6efbb76c192
batbat99 <hbatayneh@outlook.com>
2021-12-03 13:46:44 +0100

    added gradient.py

---
+++ heatmap/gradient.py
@@ -0,0 +1,17 @@
+from colour import Color, RGB_TO_COLOR_NAMES
+
+def generate_gradient(c1 , c2, n = 10):
+    """takes two colors as arguments 
+    and an optional argument for the number of samples between the two colors
+    and returns a list of colors represnting a gradient"""
+
+    color1 = Color(c1)
+    color2 = Color(c2)
+    gradient = list(color1.range_to(color2, n))
+    return gradient
+
+def get_n_colors(n):
+    """takes an integer n as an argument and returns a list of colors with length n"""
+    colors = []
+    for i in range(n):
+        colors.append(Color(pick_for = str(i)))
